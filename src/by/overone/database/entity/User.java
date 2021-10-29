24c378632928b3928d1102f588b5f921ee8f5d0c
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-10-29 16:45:27 +0300

    add getters and setters

--- src/by/overone/database/entity/User.java
+++ src/by/overone/database/entity/User.java
@@ -22,5 +22,51 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    public static int getCounter() {
+        return counter;
+    }
 
+    public static void setCounter(int counter) {
+        User.counter = counter;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
