eeafeddfaa13e8de06931d86cdf26c3de8635f8b
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-10-29 17:01:52 +0300

    deleting a user in Main

--- src/by/overone/database/Main.java
+++ src/by/overone/database/Main.java
@@ -1,7 +1,22 @@
 package by.overone.database;
 
+import by.overone.database.entity.User;
+import by.overone.database.service.UserService;
+import by.overone.database.service.impl.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+        User[] users = userService.getAllUsers();
 
+        System.out.println("REMOVE: ");
+
+        User removedUser = userService.removeUserById(10);
+
+        if(removedUser.getId() > 0){
+            System.out.println(removedUser);
+        } else{
+            System.out.println("User is not found");
+        }
     }
 }
