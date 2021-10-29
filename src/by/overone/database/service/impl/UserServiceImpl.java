62d76a10ef16ac7dad5704dc8c9170003adaa4f3
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-10-29 16:46:00 +0300

    add method realization in UserServiceImpl

--- src/by/overone/database/service/impl/UserServiceImpl.java
+++ src/by/overone/database/service/impl/UserServiceImpl.java
@@ -1,31 +1,48 @@
 package by.overone.database.service.impl;
 
+import by.overone.database.DB;
 import by.overone.database.entity.User;
+import by.overone.database.repository.UserRepository;
+import by.overone.database.repository.impl.UserRepositoryImpl;
 import by.overone.database.service.UserService;
 
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository = new UserRepositoryImpl();
+
     @Override
     public User[] getAllUsers() {
-        return new User[0];
+        return userRepository.getAllUsers();
     }
 
     @Override
     public User removeUserById(long id) {
-        return null;
+        User[] users = getAllUsers();
+        for (User user : users) {
+            if (user.getId() == id) {
+                return userRepository.removeUserById(id);
+            }
+        }
+        return new User();
     }
 
     @Override
     public User addUser(User user) {
-        return null;
+        return userRepository.addUser(user);
     }
 
     @Override
     public User getUserById(long id) {
-        return null;
+        if(id <= DB.users.size()) {
+            return userRepository.getUserById(id);
+        }
+        else{
+            return null;
+        }
     }
 
     @Override
     public User getUserByFullName(String fullName) {
-        return null;
+        return userRepository.getUserByFullName(fullName);
     }
 }
